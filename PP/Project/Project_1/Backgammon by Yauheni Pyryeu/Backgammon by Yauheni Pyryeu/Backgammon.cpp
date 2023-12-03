84ae7073cc5843a76de96da6aca1307d1ab3a8d2
ZussmanFranz <zhenya.pyryev@gmail.com>
2023-12-03 17:57:57 +0100

    HiH 1 final + PP move without highlights

--- PP/Project/Project_1/Backgammon by Yauheni Pyryeu/Backgammon by Yauheni Pyryeu/Backgammon.cpp
+++ PP/Project/Project_1/Backgammon by Yauheni Pyryeu/Backgammon by Yauheni Pyryeu/Backgammon.cpp
@@ -1,5 +1,6 @@
 ﻿#include<iostream>
 #include<curses.h>
+#include<ctime>
 
 using namespace std;
 
@@ -20,25 +21,56 @@ typedef struct pole
 	int x;
 	int y;
 	bool down;
+	int possible_moves[4];
 };
 
-void init_pola(pole *plansza_obj);
-void init_white(pole* plansza_obj);
-void init_red(pole* plansza_obj);
+typedef struct gracz
+{
+	const char* name;
+	int pionki = 15;
+	int banda = 0;
+	int dwor = 0;
+	char kolor = 'W';
+	bool real = false;
+	bool first;
+	bool quit = false;
+	int bot_type = 1;
+};
 
-void plansza(WINDOW* win, int y, int x, pole* plansza_obj); //main plansza function
-void plansza_borders(WINDOW *win, int x, int y);
-void plansza_pola(WINDOW* win, int x, int y);
-void plansza_numery(WINDOW* win, int x, int y);
-void plansza_pionki(WINDOW* win, int x, int y, pole* plansza_obj);
+void init_game(pole *plansza_obj, gracz* white, gracz* red);
+void init_white(pole* plansza_obj, gracz* white);
+void init_red(pole* plansza_obj, gracz* red);
+
+void plansza(int y, int x, pole* plansza_obj); //main plansza function
+void plansza_borders(int x, int y);
+void plansza_pola(int x, int y);
+void plansza_numery(int x, int y);
+void plansza_pionki(int x, int y, pole* plansza_obj);
 void pionek(char stan);
+void plus_pionki(int x, int y, pole* plansza_obj, int j);
+void plansza_dices(int dice1, int dice2, bool dubl);
+
+void menu_start(int y, int x, pole* plansza_obj, gracz* white, gracz* red, char kolor, int bot_level, char* game_name, int highlight);
+void menu_main(int y, int x, pole* plansza_obj, gracz* white, gracz* red, char kolor, int bot_level, char* game_name, int highlight);
+void menu_main_choice(int y, int x, pole* plansza_obj, gracz* white, gracz* red, char kolor, int bot_level, char* game_name, int highlight, int options_number);
+void menu_play(int y, int x, pole* plansza_obj, gracz* white, gracz* red, char kolor, int bot_level, char* game_name, int highlight);
+void menu_play_choice(int y, int x, pole* plansza_obj, gracz* white, gracz* red, char kolor, int bot_level, char* game_name, int highlight, int opt_number);
+void menu_play_choice_enter(int y, int x, pole* plansza_obj, gracz* white, gracz* red, char kolor, int bot_level, char* game_name, int highlight);
 
 
+void game(int y, int x, pole* plansza_obj, gracz* white, gracz* red, bool custom);
+void start_dices(int y, int x, pole* plansza_obj, gracz* first, int dice_white, int dice_red);
+void ruch_dwoch(int y, int x, pole* plansza_obj, gracz* first, gracz* second, int turn, bool custom);
+void ruch(int y, int x, pole* plansza_obj, gracz* player, int turn, bool custom);
+void ruch_commands(int y, int x, pole* plansza_obj, gracz* player, int dice1, int dice2, bool dubl, int moves, int turn, bool custom);
+void move_pionek(int y, int x, pole* plansza_obj, gracz* player);
 
 int main() {
 	initscr();
-	WINDOW* win = newwin(HEIGHT, WIDTH, 0, 0);
+	//WINDOW* win = newwin(HEIGHT, WIDTH, 0, 0);
+	//box(win, 0, 0);
 
+	srand(time(NULL)); //enables random with "random" seed
 	start_color();
 	init_pair(MATRIX_FILTER, 10, COLOR_BLACK); // 10 - Zielony kolor (jasny)
 	init_pair(WHITE_FILTER, 7, COLOR_BLACK); // 7 - Biały kolor
@@ -46,18 +78,21 @@ int main() {
 	attron(COLOR_PAIR(MATRIX_FILTER)); //color się włączył
 
 	pole plansza_obj[LICZBA_POL];
-	init_pola(plansza_obj);
+	gracz white, red;
+	white.name = "white";
+	red.name = "red";
+	char game_name[] = "default";
+	init_game(plansza_obj, &white, &red);
 
-	plansza(win, 2, 0, plansza_obj);
+	menu_start(2, 0, plansza_obj, &white, &red, 'W', 1, game_name, 0); //gra się zaczyna
 
 	attroff(COLOR_PAIR(MATRIX_FILTER)); //color się wyłączył
-	getch(); //oczekiwanie żeby program się nie zamknął od razu
 	endwin(); //żadnych argumentów!
 	return 0;
 }
 
 
-void init_pola(pole* plansza_obj)
+void init_game(pole* plansza_obj, gracz* white, gracz* red)
 {
 	for (int i = 0; i < LICZBA_POL; i++)
 	{
@@ -88,11 +123,11 @@ void init_pola(pole* plansza_obj)
 		}
 	}
 
-	init_white(plansza_obj);
-	init_red(plansza_obj);
+	init_white(plansza_obj, white);
+	init_red(plansza_obj, red);
 }
 
-void init_white(pole* plansza_obj)
+void init_white(pole* plansza_obj, gracz *white)
 {
 	plansza_obj[23].pionki = 2;
 	plansza_obj[23].stan = 'W';
@@ -105,8 +140,10 @@ void init_white(pole* plansza_obj)
 
 	plansza_obj[5].pionki = 5;
 	plansza_obj[5].stan = 'W';
+
+	(*white).kolor = 'W';
 }
-void init_red(pole* plansza_obj)
+void init_red(pole* plansza_obj, gracz *red)
 {
 	plansza_obj[0].pionki = 2;
 	plansza_obj[0].stan = 'R';
@@ -119,18 +156,21 @@ void init_red(pole* plansza_obj)
 
 	plansza_obj[18].pionki = 5;
 	plansza_obj[18].stan = 'R';
+
+	(*red).kolor = 'R';
 }
 
 
-void plansza(WINDOW* win, int y, int x, pole* plansza_obj)
+void plansza(int y, int x, pole* plansza_obj)
 {
-	plansza_borders(win, y, x);
-	plansza_pola(win, y, x);
-	plansza_numery(win, y, x);
-	plansza_pionki(win, y, x, plansza_obj);
+	clear();
+	plansza_borders(y, x);
+	plansza_pola(y, x);
+	plansza_numery(y, x);
+	plansza_pionki(y, x, plansza_obj);
+	move(y + HEIGHT + 1, x);
 }
-
-void plansza_borders(WINDOW *win, int y, int x)
+void plansza_borders(int y, int x)
 {
 	move(y, x);
 	vline(':', HEIGHT);
@@ -172,8 +212,7 @@ void plansza_borders(WINDOW *win, int y, int x)
 	move(y + HEIGHT/2, x + WIDTH);
 	printw("[HOME]");
 }
-
-void plansza_pola(WINDOW* win, int y, int x)
+void plansza_pola(int y, int x)
 {
 	for (int i = 3; i < WIDTH / 2 - 5; i += 10)
 	{
@@ -221,8 +260,7 @@ void plansza_pola(WINDOW* win, int y, int x)
 		mvvline(y + 10, x + j - 4 + WIDTH / 2, 92, 6); // 92 == '\'
 	}
 }
-
-void plansza_numery(WINDOW* win, int y, int x)
+void plansza_numery(int y, int x)
 {
 	for (int n1 = 0; n1 < 6; n1++)
 	{
@@ -242,29 +280,34 @@ void plansza_numery(WINDOW* win, int y, int x)
 		printw("%d", n2 + 7);
 	}
 }
-
-void plansza_pionki(WINDOW* win, int x, int y, pole* plansza_obj)
+void plansza_pionki(int x, int y, pole* plansza_obj)
 {
 	attroff(COLOR_PAIR(MATRIX_FILTER));
 	for (int j = 0; j < LICZBA_POL; j++)
 	{
-		for (int p = 0; p < plansza_obj[j].pionki; p++)
+		if (plansza_obj[j].pionki <= 6)
 		{
-			if (plansza_obj[j].down)
+			for (int p = 0; p < plansza_obj[j].pionki; p++)
 			{
-				move(y + plansza_obj[j].y + p, x + plansza_obj[j].x);
-				pionek(plansza_obj[j].stan);
+				if (plansza_obj[j].down)
+				{
+					move(y + plansza_obj[j].y + p, x + plansza_obj[j].x);
+					pionek(plansza_obj[j].stan);
+				}
+				else
+				{
+					move(y + plansza_obj[j].y - p, x + plansza_obj[j].x);
+					pionek(plansza_obj[j].stan);
+				}
 			}
-			else
-			{
-				move(y + plansza_obj[j].y - p, x + plansza_obj[j].x);
-				pionek(plansza_obj[j].stan);
-			}
+		}
+		else
+		{
+			plus_pionki(x, y, plansza_obj, j);
 		}
 	}
 	attron(COLOR_PAIR(MATRIX_FILTER));
 }
-
 void pionek(char stan)
 {
 	if (stan == 'W')
@@ -279,6 +322,353 @@ void pionek(char stan)
 		printw("<>");
 		attroff(COLOR_PAIR(RED_FILTER));
 	}
+}
+void plus_pionki(int x, int y, pole* plansza_obj, int j)
+{
+	for (int p = 0; p < 6; p++)
+	{
+		if (plansza_obj[j].down)
+		{
+			move(y + plansza_obj[j].y + p, x + plansza_obj[j].x);
+			pionek(plansza_obj[j].stan);
+		}
+		else
+		{
+			move(y + plansza_obj[j].y - p, x + plansza_obj[j].x);
+			pionek(plansza_obj[j].stan);
+		}
+	}
+
+	move(y + plansza_obj[j].y + 6, x + plansza_obj[j].x);
+	if (plansza_obj[j].stan == 'W')
+	{
+		attron(COLOR_PAIR(WHITE_FILTER));
+		printw("+%d", (plansza_obj[j].pionki - 6));
+		attroff(COLOR_PAIR(WHITE_FILTER));
+	}
+	else
+	{
+		attron(COLOR_PAIR(RED_FILTER));
+		printw("+%d", (plansza_obj[j].pionki - 6));
+		attroff(COLOR_PAIR(RED_FILTER));
+	}
+}
+void plansza_dices(int dice1, int dice2, bool dubl)
+{
+	printw("Dices: ");
+	if (dubl)
+	{
+		printw("%d, %d, %d, %d\n", dice1, dice1, dice1, dice1);
+	}
+	else
+	{
+		printw("%d, %d\n", dice1, dice2);
+	}
+}
+
+
+void menu_start(int y, int x, pole* plansza_obj, gracz* white, gracz* red, char kolor, int bot_level, char* game_name, int highlight)
+{
+	clear();
+	move(y, x);
+	printw("\t\t\t_    _          _____  _______     __\n");
+	printw("\t\t\t| \\ | |   /\\   |  __ \\| __  \\ \\   / /\n");
+	printw("\t\t\t|  \\| |  /  \\  | |__) | |  | \\ \\_/ /\n");
+	printw("\t\t\t| . ` | / /\\ \\ | _  / | |  | |\\   /\n");
+	printw("\t\t\t| |\\  |/ ____ \\| | \\ \\| |__| | | |\n");
+	printw("\t\t\t|_| \\_/ _ / \\_ \\_|  \\_\\_____/  |_ |\n\n");
+
+	printw("\t\t\tby Yauheni Pyryeu\n\t\t\t 201253\n\n\n");
+	printw("\t\t\tpress any key\n");
+	getch();
+	menu_main(y, x, plansza_obj, white, red, kolor, bot_level, game_name, highlight);
+}
+
+void menu_main(int y, int x, pole* plansza_obj, gracz* white, gracz* red, char kolor, int bot_level, char* game_name, int highlight)
+{
+	clear();
+	move(y, x);
+	const char* options[] = { "New game", "Load game", "Hall of fame", "Quit" };
+	int options_number = sizeof(options) / sizeof(options[0]);
+	
+	for (int i = 0; i < options_number; i++)
+	{
+		if (i == highlight)
+		{
+			attron(A_STANDOUT);
+			printw("\t%s\n", options[i]);
+			attroff(A_STANDOUT);
+		}
+		else
+		{
+			printw("\t%s\n", options[i]);
+		}
+	}
+
+	menu_main_choice(y, x, plansza_obj, white, red, kolor, bot_level, game_name, highlight, options_number);
+}
+void menu_main_choice(int y, int x, pole* plansza_obj, gracz* white, gracz* red, char kolor, int bot_level, char* game_name, int highlight, int options_number)
+{
+	char choice;
+	choice = getch();
+
+	switch (choice)
+	{
+	case 's':
+		highlight++;
+		if (highlight == options_number)
+		{
+			highlight = 0;
+		}
+		menu_main(y, x, plansza_obj, white, red, kolor, bot_level, game_name, highlight);
+		break;
+	case 'w':
+		if (highlight == 0)
+		{
+			highlight = options_number;
+		}
+		highlight--;
+		menu_main(y, x, plansza_obj, white, red, kolor, bot_level, game_name, highlight);
+		break;
+	case '\n':
+		switch (highlight)
+		{
+		case 0:
+			menu_play(y, x, plansza_obj, white, red, kolor, bot_level, game_name, highlight);
+			break;
+		case 1:
+			//load game
+			break;
+		case 2:
+			//hall of fame
+			break;
+		case 3:
+			return;
+			break;
+		default:
+			break;
+		}
+		break;
+	default:
+		break;
+	}
+}
+
+void menu_play(int y, int x, pole *plansza_obj, gracz* white, gracz* red, char kolor, int bot_level, char* game_name, int highlight)
+{
+	clear();
+	move(y, x);
+	
+	const char* options[] = {"Start", "Tornament", "Custom game", "My color: ", "Bot level: ", "Game name: ", "Quit"};
+	int opt_number = sizeof(options) / sizeof(options[0]);
+
+	for (int i = 0; i < opt_number; i++)
+	{
+		if (i == highlight)
+		{
+			attron(A_STANDOUT);
+		}
+
+		printw("\t%s", options[i]);
+
+		switch (i)
+		{
+		case 3:
+			printw("%s\n", (kolor == 'W') ? "white" : "red");
+			break;
+		case 4:
+			printw("%d\n", bot_level);
+			break;
+		case 5:
+			printw("%s\n", game_name);
+			break;
+		default:
+			printw("\n");
+			break;
+		}
+
+		if (i == highlight)
+		{
+			attroff(A_STANDOUT);
+		}
+	}
+	menu_play_choice(y, x, plansza_obj, white, red, kolor, bot_level, game_name, highlight, opt_number);
+}
+void menu_play_choice(int y, int x, pole* plansza_obj, gracz* white, gracz* red, char kolor, int bot_level, char* game_name, int highlight, int opt_number)
+{
+	char choice;
+	choice = getch();
+
+	switch (choice)
+	{
+	case 's':
+		highlight = (highlight == opt_number - 1) ? 0 : (highlight + 1);
+		menu_play(y, x, plansza_obj, white, red, kolor, bot_level, game_name, highlight);
+		break;
+	case 'w':
+		highlight = (highlight == 0) ? (opt_number - 1) : (highlight - 1);
+		menu_play(y, x, plansza_obj, white, red, kolor, bot_level, game_name, highlight);
+		break;
+	case '\n':
+		menu_play_choice_enter(y, x, plansza_obj, white, red, kolor, bot_level, game_name, highlight);
+		break;
+	default:
+		break;
+	}
+}
+void menu_play_choice_enter(int y, int x, pole* plansza_obj, gracz* white, gracz* red, char kolor, int bot_level, char* game_name, int highlight)
+{
+	switch (highlight)
+	{
+	case 0:
+		(*white).real = (kolor == 'W') ? 1 : 0;
+		(*red).real = (kolor == 'R') ? 1 : 0;
+		game(y, x, plansza_obj, white, red, false);
+		break;
+	case 1:
+		//turnej
+		break;
+	case 2:
+		//custom game
+		break;
+	case 3:
+		//my color
+		kolor = (kolor == 'R') ? 'W' : 'R';
+		menu_play(y, x, plansza_obj, white, red, kolor, bot_level, game_name, highlight);
+		break;
+	case 4:
+		//bot level
+		bot_level = (bot_level == 1) ? (bot_level + 1) : (bot_level - 1);
+		menu_play(y, x, plansza_obj, white, red, kolor, bot_level, game_name, highlight);
+		break;
+	case 5:
+		//game name
+		//printw("\tGame name: ");
+		scanw("%s", game_name);
+		menu_play(y, x, plansza_obj, white, red, kolor, bot_level, game_name, highlight);
+		break;
+	case 6:
+		//quit
+		return;
+		break;
+	default:
+		break;
+	}
+}
+
+void game(int y, int x, pole* plansza_obj, gracz* white, gracz* red, bool custom)
+{
+	int turn = 1;
+	int dice_white = (rand() % 6) + 1, dice_red = (rand() % 6) + 1;
+
+	(*white).first = (dice_white > dice_red) ? 1 : 0;
+	(*red).first = ((*white).first) ? 0 : 1;
+
+	gracz* first = ((*white).first) ? white : red;
+	gracz* second = ((*red).first) ? white : red;
+
+	start_dices(y, x, plansza_obj, first, dice_white, dice_red);
+	
+	while (((*white).pionki > 0) && ((*red).pionki > 0) && (!(*white).quit) && (!(*red).quit))
+	{
+		ruch_dwoch(y, x, plansza_obj, first, second, turn, custom);
+		turn++;
+	}
+	if ((!(*white).quit) && (!(*red).quit))
+	{
+		printw("%s player has won!\n", ((*white).pionki > 0) ? (*white).name : (*red).name);
+	}
+}
+void start_dices(int y, int x, pole* plansza_obj, gracz *first, int dice_white, int dice_red)
+{
+	plansza(y, x, plansza_obj);
+	printw("White dice: %d\n", dice_white);
+	printw("Red dice: %d\n", dice_red);
+	printw("%s goes first!\n", (*first).name);
+	getch();
+}
+void ruch_dwoch(int y, int x, pole* plansza_obj, gracz* first, gracz* second, int turn, bool custom)
+{
+	ruch(y, x, plansza_obj, first, turn, custom);
+	ruch(y, x, plansza_obj, second, turn, custom);
+}
+void ruch(int y, int x, pole* plansza_obj, gracz* player, int turn, bool custom)
+{
+	int dice1 = (rand() % 6) + 1, dice2 = (rand() % 6) + 1;
+	int moves;
+	bool dubl;
+	if (dice1 == dice2)
+	{
+		moves = 4;
+		dubl = true;
+	}
+	else
+	{
+		moves = 2;
+		dubl = false;
+	}
+
+	if ((*player).real)
+	{
+		plansza(y, x, plansza_obj);
+		printw("Turn: %d\n", turn);
+		plansza_dices(dice1, dice2, dubl);
+
+		ruch_commands(y, x, plansza_obj, player, dice1, dice2, dubl, moves, turn, custom);
+	}
+	else
+	{
+		//ruch bota
+	}
+}
+void ruch_commands(int y, int x, pole* plansza_obj, gracz* player,int dice1, int dice2, bool dubl, int moves, int turn, bool custom)
+{
+	char command;
+	printw("Your color: %s\nCommands: move (m), save (s), quit (q)\n", ((*player).kolor == 'W') ? "white" : "red");
+
+	scanw("%c", &command);
+	switch (command)
+	{
+	case 'm':
+		//possible moves check
+		for (int m = 0; m < moves; m++)
+		{
+			move_pionek(y, x, plansza_obj, player);
+			plansza(y, x, plansza_obj);
+			printw("Turn: %d\n", turn);
+			plansza_dices(dice1, dice2, dubl);
+			printw("Your color: %s\n", ((*player).kolor == 'W') ? "white" : "red");
+			//skasować wykorzystaną kostkę!!!
+		}
+		break;
+	case 's':
+		//save
+		(*player).quit = true;
+		break;
+	case 'q':
+		(*player).quit = true;
+		break;
+	default:
+		break;
+	}
+}
+void move_pionek(int y, int x, pole* plansza_obj, gracz* player)
+{
+	//legal moves check
+	char start_str[3], end_str[3];
+	int start = 0, end = 0;
+
+	printw("Enter starting position: ");
+	scanw("%d", &start);;
+	start--; //-1 for index shift
+
+	printw("Enter ending position: ");
+	scanw("%d", &end);
+	end--; //-1 for index shift
+	
+	plansza_obj[start].pionki--;
+	plansza_obj[end].pionki++;
+	plansza_obj[end].stan = plansza_obj[start].stan;
 }
 
 
