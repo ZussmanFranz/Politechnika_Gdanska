024c89891ed913eec564f3ca45f0bc9d440d535b
Zussman <zhenya.pyryev@gmail.com>
2024-05-21 22:44:49 +0200

    Started following tutorial

---
+++ PO/Project/JavaProject/src/Abstractions/KeyHandler.java
@@ -0,0 +1,30 @@
+package com.vartius.worldsim.utils;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class KeyHandler implements KeyListener {
+    private BlockingQueue<Character> keyQueue = new LinkedBlockingQueue<>();
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        char keyChar = e.getKeyChar();
+        if (keyChar == 'w' || keyChar == 'a' || keyChar == 's' || keyChar == 'd' || keyChar == 'e' || keyChar == 'q') {
+            keyQueue.offer(keyChar);
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+
+    public char getKey() throws InterruptedException {
+        return keyQueue.take();
+    }
+}
