5cc20c66337b56942e76109e3e1dbeb6ca2477d9
Zussman <zhenya.pyryev@gmail.com>
2024-05-25 17:41:24 +0200

    added quicksort + test 1 (problems with 7th test)

--- AiSD/Projects/Graphs/src/main.cpp
+++ AiSD/Projects/Graphs/src/main.cpp
@@ -4,18 +4,23 @@
 
 void parse_graph(std::vector<int> graph[], int graph_size)
 {
-    for (int i = 0; i < graph_size; i++) {
-        for (int j = 0; j < graph_size; j++) {
-            if (getchar() == '1') {
-                graph[i].push_back(j);
-            }
+    int n_edges;
+    int connected;
+
+    for (int i = 0; i < graph_size; i++) 
+    {
+        std::cin >> n_edges;
+        for (int e = 0; e < n_edges; e++) {
+            std::cin >> connected;
+            graph[i].push_back(connected);
         }
     }
     return;
 }
 
 void degree_sequence(std::vector<int> graph[], int graph_size);
-void sort_by_degree(int degrees[], int graph_size);
+int partition(int degrees[], int low, int high);
+void quickSort(int degrees[], int low, int high); 
 
 void components(std::vector<int> graph[], int graph_size); //TODO
 
@@ -36,36 +41,49 @@ void subgraphs(std::vector<int> graph[], int graph_size); //TODO
 void comlements_edges(std::vector<int> graph[], int graph_size); //TODO
 
 
-int main(int argc, char *argv[])
+int main()
 {
-    int graph_size;
+    std::ios_base::sync_with_stdio(false);
 
-    std::cin >> graph_size;
+    int n_graphs;
+    std::cin >> n_graphs;
 
-    std::vector<int>* graph = new std::vector<int> [graph_size];
-    parse_graph(graph, graph_size);
+    for (int g = 0; g < n_graphs; g++) 
+    {
+        int graph_size;
 
-    degree_sequence(graph, graph_size);
+        std::cin >> graph_size;
+
+        std::vector<int>* graph = new std::vector<int> [graph_size];
+        parse_graph(graph, graph_size);
+
+        //testing start:
+        degree_sequence(graph, graph_size);
+        
+        components(graph, graph_size);
+
+        bipartiteness(graph, graph_size);
+
+        eccentricity_sequence(graph, graph_size);
+
+        planarity(graph, graph_size);
+
+        colours_greedy(graph, graph_size);
+
+        colours_LF(graph, graph_size);
+
+        colours_SLF(graph, graph_size);
+
+        subgraphs(graph, graph_size);
+
+        comlements_edges(graph, graph_size);
+
+        delete [] graph;    
+    }
     
-    components(graph, graph_size);
-
-    bipartiteness(graph, graph_size);
-
-    planarity(graph, graph_size);
-
-    colours_greedy(graph, graph_size);
-
-    colours_LF(graph, graph_size);
-
-    colours_SLF(graph, graph_size);
-
-    subgraphs(graph, graph_size);
-
-    comlements_edges(graph, graph_size);
-
-    delete [] graph;
     return 0;
 }
+
 
 void degree_sequence(std::vector<int> graph[], int graph_size)
 {
@@ -75,7 +93,8 @@ void degree_sequence(std::vector<int> graph[], int graph_size)
         degrees[v] = graph[v].size();
     }
 
-    sort_by_degree(degrees, graph_size);
+    //sort_by_degree(degrees, graph_size);
+    quickSort(degrees, 0, graph_size - 1);
 
     for (int i = 0; i < graph_size; i++) {
         printf("%d ", degrees[i]);
@@ -87,24 +106,33 @@ void degree_sequence(std::vector<int> graph[], int graph_size)
     return;
 }
 
-void sort_by_degree(int degrees[], int graph_size)
-{
-    int temp;
-    int sorted = 0;
+int partition(int degrees[], int low, int high) {
+    int pivot = degrees[high]; // Choosing the last element as the pivot
+    int i = low - 1; // Index of the smaller element
 
-    while (sorted < graph_size) {
-        for (int i = graph_size - 2; i >= sorted; i--) {
-            if (degrees[i] < degrees[i + 1]) {
-                temp = degrees[i];
-                degrees[i] = degrees[i + 1];
-                degrees[i + 1] = temp;
-            }
+    for (int j = low; j < high; j++) {
+        // If the current element is smaller than or equal to the pivot
+        if (degrees[j] > pivot){ 
+            i++; // Increment index of smaller element
+            std::swap(degrees[i], degrees[j]);
         }
-        sorted++;
     }
 
-    return;
+    std::swap(degrees[i + 1], degrees[high]); // Swap pivot to its correct position
+    return i + 1;
 }
+
+void quickSort(int degrees[], int low, int high) {
+    if (low < high) {
+        // Partitioning index
+        int pi = partition(degrees, low, high);
+
+        // Separately sort elements before and after partition
+        quickSort(degrees, low, pi - 1);
+        quickSort(degrees, pi + 1, high);
+    }
+}
+
 
 void components(std::vector<int> graph[], int graph_size)
 {
