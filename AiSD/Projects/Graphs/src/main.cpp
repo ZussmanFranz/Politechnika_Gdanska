856c946d7e2644727ced4337684bb45dee7c9068
Zussman <zhenya.pyryev@gmail.com>
2024-05-25 20:58:21 +0200

    optimized degrees section

--- AiSD/Projects/Graphs/src/main.cpp
+++ AiSD/Projects/Graphs/src/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <vector> //must be removed!
 
-void parse_graph(std::vector<int> graph[], int graph_size)
+void parse_graph(std::vector<int> graph[], int degrees[], int graph_size)
 {
     int n_edges;
     int connected;
@@ -10,6 +10,8 @@ void parse_graph(std::vector<int> graph[], int graph_size)
     for (int i = 0; i < graph_size; i++) 
     {
         std::cin >> n_edges;
+        degrees[i] = n_edges;
+
         for (int e = 0; e < n_edges; e++) {
             std::cin >> connected;
             graph[i].push_back(connected);
@@ -18,28 +20,27 @@ void parse_graph(std::vector<int> graph[], int graph_size)
     return;
 }
 
-void degree_sequence(std::vector<int> graph[], int graph_size);
 int partition(int degrees[], int low, int high);
 void quickSort(int degrees[], int low, int high); 
 
 void components(std::vector<int> graph[], int graph_size);
 void DFS(std::vector<int> graph[], int graph_size, int current, int* checked_count, bool checked[]);
 
-void bipartiteness(std::vector<int> graph[], int graph_size); //TODO
+// void bipartiteness(std::vector<int> graph[], int graph_size); //TODO
 
-void eccentricity_sequence(std::vector<int> graph[], int graph_size); //TODO
+// void eccentricity_sequence(std::vector<int> graph[], int graph_size); //TODO
 
-void planarity(std::vector<int> graph[], int graph_size); //TODO
+// void planarity(std::vector<int> graph[], int graph_size); //TODO
 
-void colours_greedy(std::vector<int> graph[], int graph_size); //TODO
+// void colours_greedy(std::vector<int> graph[], int graph_size); //TODO
 
-void colours_LF(std::vector<int> graph[], int graph_size); //TODO
+// void colours_LF(std::vector<int> graph[], int graph_size); //TODO
 
-void colours_SLF(std::vector<int> graph[], int graph_size); //TODO
+// void colours_SLF(std::vector<int> graph[], int graph_size); //TODO
 
-void subgraphs(std::vector<int> graph[], int graph_size); //TODO
+// void subgraphs(std::vector<int> graph[], int graph_size); //TODO
 
-void comlements_edges(std::vector<int> graph[], int graph_size); //TODO
+// void comlements_edges(std::vector<int> graph[], int graph_size); //TODO
 
 
 int main()
@@ -56,56 +57,50 @@ int main()
         std::cin >> graph_size;
 
         std::vector<int>* graph = new std::vector<int> [graph_size];
-        parse_graph(graph, graph_size);
+        int *degrees = new int[graph_size];
+
+        parse_graph(graph,degrees, graph_size);
 
         //testing start:
-        degree_sequence(graph, graph_size);
+        quickSort(degrees, 0, graph_size - 1);
+        for (int i = 0; i < graph_size; i++) {
+            printf("%d ", degrees[i]);
+        }
         
-        components(graph, graph_size);
+        // components(graph, graph_size);
+        printf("\n?");
 
-        bipartiteness(graph, graph_size);
+        // bipartiteness(graph, graph_size);
+        printf("\n?");
 
-        eccentricity_sequence(graph, graph_size);
+        // eccentricity_sequence(graph, graph_size);
+        printf("\n?");
 
-        planarity(graph, graph_size);
+        // planarity(graph, graph_size);
+        printf("\n?");
 
-        colours_greedy(graph, graph_size);
+        // colours_greedy(graph, graph_size);
+        printf("\n?");
 
-        colours_LF(graph, graph_size);
+        // colours_LF(graph, graph_size);
+        printf("\n?");
 
-        colours_SLF(graph, graph_size);
+        // colours_SLF(graph, graph_size);
+        printf("\n?");
 
-        subgraphs(graph, graph_size);
+        // subgraphs(graph, graph_size);
+        printf("\n?");
 
-        comlements_edges(graph, graph_size);
+        // comlements_edges(graph, graph_size);
+        printf("\n?");
 
-        delete [] graph;    
+        delete [] graph;
+        delete [] degrees;
     }
     
     return 0;
 }
 
-
-void degree_sequence(std::vector<int> graph[], int graph_size)
-{
-    int *degrees = new int[graph_size];
-
-    for (int v = 0; v < graph_size; v++) {
-        degrees[v] = graph[v].size();
-    }
-
-    //sort_by_degree(degrees, graph_size);
-    quickSort(degrees, 0, graph_size - 1);
-
-    for (int i = 0; i < graph_size; i++) {
-        printf("%d ", degrees[i]);
-    }
-
-    printf("\n");
-
-    delete [] degrees;
-    return;
-}
 
 int partition(int degrees[], int low, int high) {
     int pivot = degrees[high]; // Choosing the last element as the pivot
@@ -182,54 +177,54 @@ void DFS(std::vector<int> graph[], int graph_size, int current, int* checked_cou
     return;
 }
 
-void bipartiteness(std::vector<int> graph[], int graph_size)
-{
-    printf("?\n");
-    return;
-}
+// void bipartiteness(std::vector<int> graph[], int graph_size)
+// {
+//     printf("?\n");
+//     return;
+// }
 
-void eccentricity_sequence(std::vector<int> graph[], int graph_size)
-{
-    printf("?\n");
-    return;
-}
+// void eccentricity_sequence(std::vector<int> graph[], int graph_size)
+// {
+//     printf("?\n");
+//     return;
+// }
 
-void planarity(std::vector<int> graph[], int graph_size)
-{
-    printf("?\n");
-    return;
-}
+// void planarity(std::vector<int> graph[], int graph_size)
+// {
+//     printf("?\n");
+//     return;
+// }
 
-void colours_greedy(std::vector<int> graph[], int graph_size)
-{
-    printf("?\n");
-    return;
-}
+// void colours_greedy(std::vector<int> graph[], int graph_size)
+// {
+//     printf("?\n");
+//     return;
+// }
 
 
-void colours_LF(std::vector<int> graph[], int graph_size)
-{
-    printf("?\n");
-    return;
-}
+// void colours_LF(std::vector<int> graph[], int graph_size)
+// {
+//     printf("?\n");
+//     return;
+// }
 
-void colours_SLF(std::vector<int> graph[], int graph_size)
-{
-    printf("?\n");
-    return;
-}
+// void colours_SLF(std::vector<int> graph[], int graph_size)
+// {
+//     printf("?\n");
+//     return;
+// }
 
-void subgraphs(std::vector<int> graph[], int graph_size)
-{
-    printf("?\n");
-    return;
-}
+// void subgraphs(std::vector<int> graph[], int graph_size)
+// {
+//     printf("?\n");
+//     return;
+// }
 
-void comlements_edges(std::vector<int> graph[], int graph_size)
-{
-    printf("?\n");
-    return;
-}
+// void comlements_edges(std::vector<int> graph[], int graph_size)
+// {
+//     printf("?\n");
+//     return;
+// }
 
 
 
