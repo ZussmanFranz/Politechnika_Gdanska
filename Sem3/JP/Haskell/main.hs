a7452c1151f9e409b0e9490f237c2545515892c7
Zussman <zhenya.pyryev@gmail.com>
2024-12-11 23:46:05 +0100

    Haskell project add

--- Sem3/JP/Haskell/main.hs
+++ Sem3/JP/Haskell/main.hs
@@ -1,69 +1,77 @@
-import Data.Char (intToDigit)
-import Data.List (subsequences)
-
 --                                          49
--- if n = a^2 + k*b^2
-canBeRepresented :: Integer -> Integer -> Bool
-canBeRepresented n k = any (\a -> let b2 = n - a*a in b2 >= 0 && b2 `mod` k == 0 && isSquare (b2 `div` k)) [0..floor (sqrt (fromIntegral n))]
+-- if n = a^2 + k*(b^2)
+isSumOfSquares :: Int -> Int -> Bool
+isSumOfSquares n k = any (\a -> let b2 = n - a*a in b2 >= 0 && b2 `mod` k == 0 && isSquare (b2 `div` k)) [0..floor (sqrt (fromIntegral n))]
   where
     isSquare x = let s = floor (sqrt (fromIntegral x)) in s * s == x
 
-findNumbers :: Integer -> [Integer]
-findNumbers n = filter (\x -> canBeRepresented x 1 &&
-                               canBeRepresented x 2 &&
-                               canBeRepresented x 3 &&
-                               canBeRepresented x 7) [1..n]
+findNums :: Int -> [Int]
+findNums n = filter (\x -> isSumOfSquares x 1 &&
+                              isSumOfSquares x 2 &&
+                              isSumOfSquares x 3 &&
+                              isSumOfSquares x 7) [1..n]
 
 -- count the numbers
-countNumbers :: Integer -> Int
-countNumbers n = length (findNumbers n)
+countNums :: Int -> Int
+countNums n = length (findNums n)
+
 
 --                                          22
+genDecPalindromes :: Int -> [Int]
+genDecPalindromes n = [i | i <- [1..n], show i == reverse (show i)]
+
+-- number to bin
+toBinary :: Int -> String
+toBinary 0 = "0"
+toBinary n = toBinary' n []
+  where
+    toBinary' 0 bs = bs
+    toBinary' n bs = toBinary' (n `div` 2) ((if n `mod` 2 == 0 then '0' else '1') : bs)
+
+-- string is a palindrome
 isPalindrome :: String -> Bool
 isPalindrome s = s == reverse s
 
--- num to bin
-toBinary :: Integer -> String
-toBinary n = map intToDigit $ reverse $ toBinary' (fromInteger n) []
-  where
-    toBinary' :: Int -> [Int] -> [Int]
-    toBinary' 0 acc = acc
-    toBinary' n' acc = toBinary' (n' `div` 2) ((n' `mod` 2) : acc)
+genPalindromes:: Int -> [Int]
+genPalindromes n = filter (\i -> isPalindrome (toBinary i)) (genDecPalindromes n)
 
-sumDecimalBinaryPalindromes :: Integer -> Integer
-sumDecimalBinaryPalindromes n = sum $ filter isDecimalBinaryPalindrome [1..n]
-  where
-    isDecimalBinaryPalindrome x = isPalindrome (show x) && isPalindrome (toBinary x)
+sumOfPalindromes:: Int -> Int
+sumOfPalindromes n = sum (genPalindromes n)
 
---                                          31
-hasEqualSumDisjointSubsets :: [Integer] -> Bool
-hasEqualSumDisjointSubsets xs = any (\k -> hasEqualSumSubsetsOfSize k xs) [1..(length xs `div` 2)]
-  where
-    hasEqualSumSubsetsOfSize k xs =
-      let subsets = filter (\s -> length s == k) (subsequences xs)
-      in any (\s1 -> any (\s2 -> s1 /= s2 && sum s1 == sum s2 && null (s1 `intersect` s2)) subsets) subsets
-      where
-        intersect [] _ = []
-        intersect _ [] = []
-        intersect (x:xs) ys
-            | x `elem` ys = x : intersect xs ys
-            | otherwise   = intersect xs ys
 
--- Example usage
+
+--                                31
+--- get all possible subsets of a set
+getSubsets :: [a] -> [[a]]
+getSubsets [] = [[]]
+getSubsets (x:xs) = getSubsets xs ++ map (x:) (getSubsets xs)
+
+-- if two lists are disjoint
+disjoint :: Eq a => [a] -> [a] -> Bool
+disjoint [] _ = True
+disjoint _ [] = True
+disjoint (x:xs) (y:ys)
+  | x == y = False
+  | otherwise = disjoint (x:xs) ys && disjoint xs (y:ys)
+
+hasSubsets :: [Int] -> Bool
+hasSubsets setA1 = any (\(b, c) -> sum b == sum c && disjoint b c && not (null b) && not (null c)) pairs
+  where
+    subsets = getSubsets setA1
+    pairs = [(b, c) | b <- subsets, c <- subsets, b /= c]
+
+--                              main
 main :: IO ()
 main = do
   putStrLn "49: Enter n:"
-  input <- getLine
-  let n = read input :: Integer
-  print (countNumbers n)
+  n <- readLn :: IO Int
+  print (countNums n)
 
   putStrLn "22: Enter n:"
-  input <- getLine
-  let n = read input :: Integer
-  print (sumDecimalBinaryPalindromes n)
+  n <- readLn :: IO Int
+  print(sumOfPalindromes n)
 
-  putStrLn "31: Enter setA1: "
-  -- example: 81 88 75 42 87 84 86 65
+  putStrLn "31: Enter set: "
   input <- getLine
-  let setA1 = map read $ words input :: [Integer]
-  print (hasEqualSumDisjointSubsets (setA1))
+  let setA1 = map read $ words input :: [Int]
+  print (hasSubsets setA1)
