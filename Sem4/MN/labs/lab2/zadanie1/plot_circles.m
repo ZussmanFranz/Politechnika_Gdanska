453b79ef9940eef0e21b1019749b1fff96fbe64a
Zussman <zhenya.pyryev@gmail.com>
2025-03-13 14:34:32 +0100

    lab2 zadanie1 zrobione (TODO: refactor)

--- Sem4/MN/labs/lab2/zadanie1/plot_circles.m
+++ Sem4/MN/labs/lab2/zadanie1/plot_circles.m
@@ -18,3 +18,12 @@ function plot_circles(a, b, circles)
     hold off
 end
     
+function plot_circle(X, Y, R)
+    % R - promień okręgu
+    % X - współrzędna x środka okręgu
+    % Y - współrzędna y środka okręgu
+    theta = linspace(0,2*pi);
+    x = R*cos(theta) + X;
+    y = R*sin(theta) + Y;
+    plot(x,y)
+end
