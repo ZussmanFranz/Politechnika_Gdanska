bc548972b9790356e25fe01bbe7e2e1c34890720
Zussman <zhenya.pyryev@gmail.com>
2025-03-31 22:59:09 +0200

    SWiM lab4 without additional task

--- Sem4/SWiM/lab4/main.cpp
+++ Sem4/SWiM/lab4/main.cpp
@@ -2,21 +2,9 @@
 #include <i2c_BMP280.h>
 
 #define UPDATE_FREQUENCY 1000;
-#define CRITICAL_TEMPERATURE_HIGH 26.3
-#define CRITICAL_TEMPERATURE_LOW 26.2
 
 LiquidCrystal LCD(12, 11, 5, 4, 3, 2);
 BMP280 bmp280;
-
-void critical_notification(char symbol) {
-
-    LCD.setCursor(15, 1);
-    LCD.print(symbol);
-    delay(500);
-    LCD.setCursor(15, 1);
-    LCD.print("*");
-
-}
 
 void update_measurements() {
 
@@ -40,16 +28,6 @@ void update_measurements() {
     LCD.print("C");
 
     LCD.setCursor(0, 0);
-
-    // If the temperature is too high/low - show a blinking notification in
-    // the bottom right corner of the LCD screen
-    if (temperature > CRITICAL_TEMPERATURE_HIGH) critical_notification('H');
-    else if (temperature < CRITICAL_TEMPERATURE_LOW) critical_notification('L');
-    else {
-        LCD.setCursor(15, 1);
-        LCD.print(" ");
-    }
-
 }
 
 void setup() {
