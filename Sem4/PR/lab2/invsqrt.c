be7252f9713c48d77a4ec357c12acddf4efccfac
Zussman <zhenya.pyryev@gmail.com>
2025-04-07 14:22:34 +0200

    PR lab2 normal_invsqrt and fast_invsqrt implemented

--- Sem4/PR/lab2/invsqrt.c
+++ Sem4/PR/lab2/invsqrt.c
@@ -1,12 +1,28 @@
 #include <math.h>
+#include <stdint.h>
 #include <stdio.h>
 
-float normal_invsqrt(int number){
-    return (1 / sqrt(number));
+// Default implementation of inverted square root using math function:
+float normal_invsqrt(float number){
+    return (1 / sqrtf(number));
+}
+
+// "Fast inverse square root" implementation
+float fast_invsqrt(float number){
+    union {
+        float    f;
+        uint32_t i;
+    } conv = { .f = number };
+
+    conv.i  = 0x5f3759df - (conv.i >> 1);
+    conv.f *= 1.5F - (number * 0.5F * conv.f * conv.f);
+    
+    return conv.f;
 }
 
 int main(){
-    printf("1 / sqrt(25) = %f\n", normal_invsqrt(25));
+    printf("1 / sqrt(25) = %f\n", normal_invsqrt(2535234));
+    printf("1 / sqrt(25) = %f\n", fast_invsqrt(2535234));
 
     return 0;
 }
