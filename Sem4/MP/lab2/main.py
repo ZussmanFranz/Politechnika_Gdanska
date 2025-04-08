99ab5c3646319675f20f91f3c72c3ff825c73cc5
Zussman <zhenya.pyryev@gmail.com>
2025-04-08 18:20:49 +0200

    MP lab2 done

--- Sem4/MP/lab2/main.py
+++ Sem4/MP/lab2/main.py
@@ -1,130 +1,100 @@
-class LinearGenerator:
-    def __init__(self, a: int, c: int, m: int, seed: int = 1):
-        self.a = a
-        self.c = c
-        self.m = m
-        self.seed = seed
+import numpy as np
+# import matplotlib.pyplot as plt 
 
-    def __generate_number(self) -> int:
-        self.seed = (self.a * self.seed + self.c) % self.m
-        return self.seed
+# Zadanie 1: Xn+1 = (a * Xn + c) mod M
 
-    def generate_probability(self) -> float:
-        return self.__generate_number() / self.m
-
-    def generate_probabilities(self, n: int):
-        for _ in range(n):
-            yield self.generate_probability()
-
-    def generate_number(self, m: int = None) -> int:
-        if m is None:
-            m = self.m
-        return int(m * self.generate_probability())
-
-    def generate_numbers(self, n: int, m: int = None):
-        if m is None:
-            m = self.m
-        for _ in range(n):
-            yield self.generate_number(m)
-
-class RegisterGenerator:
-    def __init__(self, p: int, q: int, seed: int = 2**31, accuracy: int = 31):
-        self.p = p
-        self.q = q
-        self.size = p+q
-        self.seed = seed
-        self.accuracy = accuracy
-
-    def __generate_bit(self) -> int:
-        p_bit = (self.seed >> (self.p-1)) & 1
-        q_bit = (self.seed >> (self.q-1)) & 1
-        bit = p_bit ^ q_bit
-        self.seed >>= 1
-        self.seed |= bit << self.size
-        return bit
-
-    def generate_probability(self) -> float:
-        result = 0
-        weight = 0.5
-        for _ in range(self.accuracy):
-            if self.__generate_bit() == 1:
-                result += weight
-            weight /= 2
-        return result
-
-    def generate_probabilities(self, n: int):
-        for _ in range(n):
-            yield self.generate_probability()
-
-    def generate_number(self, m: int = None) -> int:
-        if m is None:
-            m = 2**self.accuracy
-        return int(m * self.generate_probability())
-
-    def generate_numbers(self, n: int, m: int = None):
-        if m is None:
-            m = 2**self.accuracy
-        for _ in range(n):
-            yield self.generate_number(m)
-
-class Bucket:
-    def __init__(self, min: int, max: int, count: int = 0):
-        self.min = min
-        self.max = max
-        self.count = count
-
-    def __str__(self):
-        return f"[{self.min} - {self.max}] {self.count}"
-
-def split_into_buckets(data: list[int], bucket_count: int, max_value: int = 2**31) -> list:
-    per_bucket = int(max_value / bucket_count)
-
-    buckets = [Bucket(i*per_bucket, (i+1)*per_bucket) for i in range(bucket_count)]
-
-    for number in data:
-        bucket_id = number // per_bucket
-        buckets[bucket_id].count += 1
-    return buckets
-
-
+Xo = 15
 a = 69069
 c = 1
-m = 2**31
+M = 2**31
 
+n = 1000000
+N_counters = 10
+
+def generateX(Xprev):
+    global a, c, M
+    result = (Xprev * a + c) % M
+
+    # returns (number, next_element_base)
+    return (result, result)
+
+def generateNextBit(bits):
+    global p, q
+    bitp = int(bits[-p])
+    bitq = int(bits[-q])
+    return str(bitp ^ bitq)
+
+def generateBitsNumber(start_bits):
+    global size
+    str_number = start_bits
+    n_generated = len(str_number)
+
+    while n_generated < size:
+        str_number += generateNextBit(str_number)
+        n_generated += 1
+
+    # returns (number, next_element_base)
+    return (int(str_number, 2), str_number[-len(start_bits):])
+
+def generateN(generate_func, n, start_value):
+    numbers = []
+    
+    base = start_value
+    for _ in range(0, n):
+        (val, base) = generate_func(base)
+        numbers.append(val)
+
+    return numbers
+
+def evaluateRandom(numbers, N_counters, max_val):
+    counters = np.zeros(N_counters)
+
+    counters_thresholds = np.zeros(N_counters)
+    step = max_val / N_counters
+
+    for i in range(1, N_counters + 1):
+        counters_thresholds[i - 1] = step * i
+
+    for val in numbers:
+        for counter_id in range(0, N_counters):
+            if val <= counters_thresholds[counter_id]:
+                counters[counter_id] += 1
+                break
+
+    return counters
+
+def generateBinaryNumbers(start_bits, count):
+    global q
+    binary_numbers = []
+    current_bits = start_bits
+    
+    for _ in range(count):
+        binary_numbers.append(int(current_bits, 2))
+        next_bits = ''
+        for _ in range(len(current_bits)):
+            next_bits += generateNextBit(current_bits)
+            current_bits = current_bits[1:] + next_bits[-1]
+        current_bits = current_bits[-q:]
+    
+    return binary_numbers
+
+# Generujemy n liczb, potem badamy rownomiernosc przez podzial zakresu (0:M-1) na N_licznikow i liczymy, do ktorego przedzialu nalezy liczba 
+
+numbers = generateN(generateX, n, Xo)
+countersX = evaluateRandom(numbers, N_counters, M)
+print(countersX)
+
+# Zadanie 2: bi = bi-p xor bi-q
+
+start_bits = '1101101'
 p = 7
 q = 3
+size = 31
 
-seed = 15
-N = 100_000
-buckets = 10
+numbers = generateN(generateBitsNumber, n, start_bits)
+countersB = evaluateRandom(numbers, N_counters, 2**size)
+# print(numbers)
+print(countersB)
 
-print(f"a = {a}, c = {c}, m = {m}\np = {p}, q = {q}\nseed = {seed}, N = {N}, buckets = {buckets}")
-
-
-linear_generator = LinearGenerator(a, c, m, seed)
-register_generator = RegisterGenerator(p, q, seed)
-
-linear_numbers = list(linear_generator.generate_numbers(N, m))
-generator_numbers = list(register_generator.generate_numbers(N, m))
-
-print(f"linear average = {sum(linear_numbers)/N}")
-print(f"generator average = {sum(generator_numbers)/N}")
-
-linear_buckets = split_into_buckets(linear_numbers, buckets, m)
-generator_buckets = split_into_buckets(generator_numbers, buckets, m)
-
-print("\nLinear buckets:")
-for bucket in linear_buckets:
-    print(bucket)
-
-print("\nGenerator buckets:")
-for bucket in generator_buckets:
-    print(bucket)
-
-
-# linear_probabilities = list(linear_generator.generate_probabilities(N))
-# register_probabilities = list(register_generator.generate_probabilities(N))
-
-# print(sum(linear_probabilities)/N)
-# print(sum(register_probabilities)/N)
+# print(f'{numbers[0]} in binary: {bin(numbers[0])}')
 
