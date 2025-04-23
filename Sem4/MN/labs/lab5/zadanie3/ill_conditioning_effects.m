249c9f793f87bd8ca9e8e95bb016f5bea0c29212
Zussman <zhenya.pyryev@gmail.com>
2025-04-23 23:04:27 +0200

    MN lab5 zadanie3 done

--- Sem4/MN/labs/lab5/zadanie3/ill_conditioning_effects.m
+++ Sem4/MN/labs/lab5/zadanie3/ill_conditioning_effects.m
@@ -12,90 +12,106 @@ function [matrix_sizes, condition_numbers, interpolation_error_exact, interpolat
 %       a obliczonymi współczynnikami wielomianu, gdy b zawiera
 %       zaburzone wartości funkcji kwadratowej 
 
+    % Inicjalizacja danych
+    matrix_sizes = 5:100;
+    num_points = length(matrix_sizes);
 
-% Inicjalizacja danych
-matrix_sizes = 5:100;
-num_points = length(matrix_sizes);
+    % Prealokacja danych
+    condition_numbers = zeros(1, num_points);
+    interpolation_error_exact = zeros(1, num_points);
+    interpolation_error_perturbed = zeros(1, num_points);
 
-% Prealokacja danych
-condition_numbers = zeros(1, num_points);
-interpolation_error_exact = zeros(1, num_points);
-interpolation_error_perturbed = zeros(1, num_points);
+    %===========================================================================
+    % Część 1: Obliczanie współczynnika uwarunkowania macierzy Vandermonde'a
+    %===========================================================================
+    for index = 1:num_points
+        size_n = matrix_sizes(index);
+        interpolation_nodes = linspace(-1, 1, size_n)';
+        V = get_vandermonde_matrix(interpolation_nodes);
 
-%===========================================================================
-% Część 1: Obliczanie współczynnika uwarunkowania macierzy Vandermonde'a
-%===========================================================================
-for index = 1:num_points
-    size_n = matrix_sizes(index);
-    interpolation_nodes = linspace(-1, 1, size_n)';
-    V = []; % TODO
+            condition_numbers(index) = cond(V);
+    end
 
-    % condition_numbers(index) = % TODO
-end
+    % Szukam progu złego uwarunkowania
+    threshold_index = find(condition_numbers >= 1e8, 1);
 
-% Szukam progu złego uwarunkowania
-threshold_index = find(condition_numbers >= 1e8, 1);
+        % Wykres 1
+        tiledlayout(3, 1, 'Padding', 'compact', 'TileSpacing', 'compact');
+        nexttile;
+        semilogy(matrix_sizes, condition_numbers, 'b.-')
+        title('Współczynnik uwarunkowania macierzy Vandermonde-a');
+        xlabel('Rozmiar macierzy');
+        ylabel('cond(V)');
+        grid on;
 
-% Wykres 1
-tiledlayout(3, 1, 'Padding', 'compact', 'TileSpacing', 'compact');
-nexttile;
-% semilogy(matrix_sizes,  % TODO
+        if ~isempty(threshold_index)
+            size_threshold = matrix_sizes(threshold_index);
+            xline(size_threshold, '--', 'cond(V) > 10^8', 'LabelOrientation', ...
+                'horizontal', 'LabelVerticalAlignment', 'top', ...
+                'LabelHorizontalAlignment', 'left', 'Color', [0.494 0.184 0.556]);
+        end
 
-if ~isempty(threshold_index)
-    size_threshold = matrix_sizes(threshold_index);
-    xline(size_threshold, '--', 'cond(V) > 10^8', 'LabelOrientation',...
-        'horizontal', 'LabelVerticalAlignment', 'top',...
-        'LabelHorizontalAlignment', 'left', 'Color', [0.494 0.184 0.556]);
-end
-% TODO
+    %===========================================================================
+    % Część 2: Obliczenie błędu interpolacji dla dokładnych danych (f(x)=x^2)
+    %===========================================================================
+    for index = 1:num_points
+        size_n = matrix_sizes(index);
+        interpolation_nodes = linspace(-1, 1, size_n)';
+        V = get_vandermonde_matrix(interpolation_nodes);
 
-%===========================================================================
-% Część 2: Obliczenie błędu interpolacji dla dokładnych danych (f(x)=x^2)
-%===========================================================================
-for index = 1:num_points
-    size_n = matrix_sizes(index);
-    interpolation_nodes = linspace(-1, 1, size_n)';
-    V = []; % TODO
+        a2 = 1;
+        b_exact = a2*interpolation_nodes.^2; % f(x) = a2*x^2
+    
+        reference_coefficients = [0; 0; a2; zeros(size_n - 3, 1)];
+        computed_coefficients = V \ b_exact;
 
-    a2 = 1;
-    b_exact = a2*interpolation_nodes.^2; % f(x) = a2*x^2
-    % TODO:
-    reference_coefficients = [0; 0; a2; zeros(size_n - 3, 1)];
-    % computed_coefficients =
+        interpolation_error_exact(index) = max(abs(computed_coefficients - reference_coefficients));
+    end
 
-    % interpolation_error_exact(index) =...
-        % max(abs(computed_coefficients - reference_coefficients));
-end
+    % Wykres 2
+        nexttile;
+        plot(matrix_sizes, interpolation_error_exact)
+        title('Błąd interpolacji dla dokładnych danych');
+        xlabel('Rozmiar macierzy');
+        ylabel('Błąd współczynnika');
+        grid on;
 
-% Wykres 2
-nexttile;
-% plot(matrix_sizes, % TODO
 
-%===========================================================================
-% Część 3: Obliczenie błędu interpolacji dla danych zaburzonych
-%===========================================================================
-for index = 1:num_points
-    size_n = matrix_sizes(index);
-    interpolation_nodes = linspace(-1, 1, size_n)';
-    V = []; % TODO
+    %===========================================================================
+    % Część 3: Obliczenie błędu interpolacji dla danych zaburzonych
+    %===========================================================================
+    for index = 1:num_points
+        size_n = matrix_sizes(index);
+        interpolation_nodes = linspace(-1, 1, size_n)';
+        V = get_vandermonde_matrix(interpolation_nodes);
 
-    a2 = 1;
-    b_perturbed = a2*interpolation_nodes.^2 + rand(size_n, 1) * 1e-9;
-    % TODO:
-    % reference_coefficients = 
-    % computed_coefficients =
+        a2 = 1;
+        b_perturbed = a2 * interpolation_nodes.^2 + randn(size_n, 1) * 1e-9;
 
-    % interpolation_error_perturbed(index) =...
-        % max(abs(computed_coefficients - reference_coefficients));
-end
+        reference_coefficients = [0; 0; a2; zeros(size_n - 3, 1)];
+        computed_coefficients = V \ b_perturbed;
 
-% Wykres 3
-nexttile;
-% semilogy(matrix_sizes, % TODO
+        interpolation_error_perturbed(index) = max(abs(computed_coefficients - reference_coefficients));
+    end
+
+
+    % Wykres 3
+    nexttile;
+    semilogy(matrix_sizes, interpolation_error_perturbed, 'g.-');
+    title('Błąd interpolacji dla danych zaburzonych (szum 10^{-9})');
+    xlabel('Rozmiar macierzy');
+    ylabel('Maksymalny błąd współczynnika');
+    grid on;
+
+    % Zapis do pliku
+    saveas(gcf, 'zadanie3.png');
 end
 
 function V = get_vandermonde_matrix(x)
-% Buduje macierz Vandermonde’a na podstawie wektora węzłów interpolacji x.
-N = length(x);
-V = []; % TODO
+% Buduje macierz Vandermonde’a dla węzłów x
+    N = length(x);
+    V = zeros(N, N);
+    for i = 1:N
+        V(:, i) = x.^(i-1);
+    end
 end
