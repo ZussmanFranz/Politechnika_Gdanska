24dec5b7a2b98dce230db5f6380d7147e78377e9
Zussman <zhenya.pyryev@gmail.com>
2025-04-23 22:39:04 +0200

    PT lab5 + MN lab5 zadanie2 done

--- Sem4/MN/labs/lab5/zadanie2/compare_uniform_vs_chebyshev.m
+++ Sem4/MN/labs/lab5/zadanie2/compare_uniform_vs_chebyshev.m
@@ -13,73 +13,72 @@ function [N, x_uniform, y_fine_uniform, x_chebyshev, y_fine_chebyshev] = ...
 %
 % Oznaczonie TODO wskazuje fragmenty kodu wymagające zmian.
 
-N = 3; % liczba węzłów interpolacji TODO
+N = 16; % wybrana liczba węzłów interpolacji (z przedziału [12,20])
 
 % Gęsta siatka punktów do testowania interpolacji
 x_fine = linspace(-1, 1, 1000);
-
-% funkcja Rungego
 runge_function = @(x) 1 ./ (1 + 25 * x.^2);
 y_fine_reference = runge_function(x_fine);
 
 % 1. węzły równomiernie rozmieszczone
 x_uniform = linspace(-1, 1, N);
-% obliczenie wartości funkcji w węzłach
 y_uniform = runge_function(x_uniform);
-% wyznaczenie macierzy Vandermonde'a
 V_uniform = get_vandermonde_matrix(x_uniform);
-% wyznaczenie współczynników wielomianu interpolacyjnego
-coeff_uniform = []; % TODO
-% odwrócenie kolejności wsp. wielomianów: dostosowanie do polyval
-coeff_uniform = coeff_uniform(end:-1:1);
-% wyznaczenie wartości wielomianu w punktach testowych
+coeff_uniform = V_uniform \ y_uniform.'; % rozwiązywanie układu równań
+coeff_uniform = coeff_uniform(end:-1:1); % dostosowanie do polyval
 y_fine_uniform = polyval(coeff_uniform, x_fine);
 
 % 2. węzły Czebyszewa II rodzaju
 x_chebyshev = get_chebyshev_nodes(N);
-y_chebyshev = []; % TODO
-V_chebyshev = []; % TODO
-coeff_chebyshev = []; % TODO
-coeff_chebyshev = coeff_chebyshev (end:-1:1);
-y_fine_chebyshev = []; % TODO
+y_chebyshev = runge_function(x_chebyshev);
+V_chebyshev = get_vandermonde_matrix(x_chebyshev);
+coeff_chebyshev = V_chebyshev \ y_chebyshev.';
+coeff_chebyshev = coeff_chebyshev(end:-1:1);
+y_fine_chebyshev = polyval(coeff_chebyshev, x_fine);
 
 % 3. Wykresy
+figure;
+
+% Wykres dla węzłów równomiernych
 subplot(2,1,1);
 plot(x_fine, y_fine_reference, 'k--', 'LineWidth', 2, 'DisplayName', 'Funkcja wzorcowa');
 hold on;
-% TODO:
+plot(x_fine, y_fine_uniform, 'm', 'DisplayName', ['Interpolacja N = ', num2str(N)]);
+plot(x_uniform, y_uniform, 'mo', 'DisplayName', 'Wartości w węzłach');
+legend('Location', 'eastoutside');
+xlabel('x');
+ylabel('y');
+title('Interpolacja: Węzły równomierne');
 
-% plot(x_fine, y_fine_uniform, 'm', 'DisplayName', ['Interpolacja N = ', num2str(N)]);
-% plot(x_uniform, y_uniform, 'mo', 'DisplayName', ['Wartości w węzłach. N = ', num2str(N)]);
-% hold off ;
-% legend show;
-% legend('Location', 'eastoutside')
-% xlabel('x');
-% ylabel('y');
-
+% Wykres dla węzłów Czebyszewa
 subplot(2,1,2);
 plot(x_fine, y_fine_reference, 'k--', 'LineWidth', 2, 'DisplayName', 'Funkcja wzorcowa');
 hold on;
-% TODO:
+plot(x_fine, y_fine_chebyshev, 'b', 'DisplayName', ['Interpolacja N = ', num2str(N)]);
+plot(x_chebyshev, y_chebyshev, 'bo', 'DisplayName', 'Wartości w węzłach');
+legend('Location', 'eastoutside');
+xlabel('x');
+ylabel('y');
+title('Interpolacja: Węzły Czebyszewa');
 
-% plot(x_fine, y_fine_chebyshev, 'b', 'DisplayName', ['Interpolacja N = ', num2str(N)]);
-% plot(x_chebyshev, y_chebyshev, 'bo', 'DisplayName', ['Wartości w węzłach. N = ', num2str(N)]);
-% hold off ;
-% legend show;
-% legend('Location', 'eastoutside')
-% xlabel('x');
-% ylabel('y');
+set(gcf, 'Position', [1000 500 2000 1500]);
 
-% set(gcf, 'Position', [1000 500 2000 1500]);
+% Zapis wykresu do pliku
+saveas(gcf, 'zadanie2.png');
 end
 
 function x = get_chebyshev_nodes(N)
-% Węzły Czebyszewa drugiego rodzaju wyznaczone w przedziale [-1, 1]
-x = []; % TODO
+    % Węzły Czebyszewa drugiego rodzaju w przedziale [-1, 1]
+    k = 0:(N-1);
+    x = cos(pi * k / (N - 1));
+    x = x(:)'; % upewniamy się, że x to wektor wierszowy
 end
-
+    
+% --- Funkcja pomocnicza: macierz Vandermonde’a ---
 function V = get_vandermonde_matrix(x)
-% Buduje macierz Vandermonde’a na podstawie wektora węzłów interpolacji x.
-N = length(x);
-V = []; % TODO
+    N = length(x);
+    V = zeros(N, N);
+    for i = 1:N
+        V(:, i) = x.^(i - 1);
+    end
 end
