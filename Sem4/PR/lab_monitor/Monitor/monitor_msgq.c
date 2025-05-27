86e880bf0d3ab4fbca3d2117a3e0cfd7cb74577f
Zussman <zhenya.pyryev@gmail.com>
2025-05-27 09:41:05 +0200

    Monitor changed demo + fixed monitor_notify

--- Sem4/PR/lab_monitor/Monitor/monitor_msgq.c
+++ Sem4/PR/lab_monitor/Monitor/monitor_msgq.c
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <errno.h>
+#include <sys/shm.h>
 
 #define MONITOR_KEY 2345L
 #define ENTRY_MTYPE 1L
@@ -63,6 +64,9 @@ void monitor_notify() {
     struct msg_buffer msg;
     if (msgrcv(monitor_id, &msg, sizeof(int), WAIT_MTYPE, IPC_NOWAIT) != -1) {
         msgsnd(monitor_id, &msg, sizeof(int), 0);
+    } else {
+        // No one is waiting, release the monitor for the next process
+        exit_monitor();
     }
 }
 
@@ -73,23 +77,47 @@ void monitor_destroy() {
     }
 }
 
-// Monitor demo
-void monitor_func(int *counter, int use_wait) {
+// Attach to shared memory for counter
+int *get_shared_counter(int *shmid_out) {
+    int shmid = shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT | 0666);
+    if (shmid == -1) {
+        perror("shmget");
+        exit(1);
+    }
+    if (shmid_out) *shmid_out = shmid;
+    int *ptr = (int*)shmat(shmid, NULL, 0);
+    if (ptr == (void*)-1) {
+        perror("shmat");
+        exit(1);
+    }
+    *ptr = 0;
+    return ptr;
+}
+
+void detach_and_remove_shm(int *ptr, int shmid) {
+    shmdt(ptr);
+    shmctl(shmid, IPC_RMID, NULL);
+}
+
+// Updated monitor_func for shared counter and process id
+void monitor_func(int *counter, int proc_id, int use_wait, int do_notify) {
     enter_monitor();
-    printf("Process %d is in monitor. Counter: %d\n", getpid(), *counter);
+    printf("[PID %d] Proc %d: in monitor. Counter: %d\n", getpid(), proc_id, *counter);
     if (use_wait && *counter == 0) {
-        printf("Process %d waits\n", getpid());
+        printf("[PID %d] Proc %d: waits (monitor_wait)\n", getpid(), proc_id);
         monitor_wait();
-        printf("Process %d got back from wait\n", getpid());
+        printf("[PID %d] Proc %d: got back from wait\n", getpid(), proc_id);
     }
     (*counter)++;
+    printf("[PID %d] Proc %d: incremented counter to %d\n", getpid(), proc_id, *counter);
     sleep(1);
-    printf("Process %d is finished. Counter: %d\n", getpid(), *counter);
-    if (use_wait) {
+    if (do_notify) {
+        printf("[PID %d] Proc %d: notifies (monitor_notify)\n", getpid(), proc_id);
         monitor_notify();
     } else {
         exit_monitor();
     }
+    printf("[PID %d] Proc %d: finished.\n", getpid(), proc_id);
 }
 
 int main() {
@@ -98,15 +126,31 @@ int main() {
         return 1;
     }
     atexit(monitor_destroy);
-    int counter = 0;
-    pid_t pid = fork();
-    if (pid == 0) {
-        monitor_func(&counter, 1); // first process uses wait/notify
-        _exit(0);
-    } else {
-        sleep(1);
-        monitor_func(&counter, 0); // second only increments
-        wait(NULL);
+    int shmid;
+    int *counter = get_shared_counter(&shmid);
+    int n = 4; // number of processes
+    pid_t pids[4];
+    for (int i = 0; i < n; ++i) {
+        pids[i] = fork();
+        if (pids[i] < 0) {
+            perror("fork");
+            // Clean up and exit if fork fails
+            for (int j = 0; j < i; ++j) waitpid(pids[j], NULL, 0);
+            detach_and_remove_shm(counter, shmid);
+            return 1;
+        }
+        if (pids[i] == 0) {
+            // Child process: break the loop so only one child runs its code
+            int use_wait = (i == 0);
+            int do_notify = (i == 1);
+            monitor_func(counter, i, use_wait, do_notify);
+            shmdt(counter);
+            _exit(0);
+        }
     }
+    // Parent waits for all
+    for (int i = 0; i < n; ++i) waitpid(pids[i], NULL, 0);
+    printf("[Parent] Final counter value: %d\n", *counter);
+    detach_and_remove_shm(counter, shmid);
     return 0;
 }
