20ebcbb19fac83f92057fa6b860170299c8b50e3
Zussman <zhenya.pyryev@gmail.com>
2025-05-29 15:04:33 +0200

    zadanie5 started

---
+++ Sem4/MN/labs/lab7/zadanie5/compute_lake_volume_monte_carlo.m
@@ -0,0 +1,18 @@
+function [x, y, z, zmin, lake_volume] = compute_lake_volume_monte_carlo()
+    % Wyznacza objętość jeziora metodą Monte Carlo.
+    %
+    % x/y/z - wektory wierszowe, które zawierają współrzędne x/y/z punktów
+    %       wylosowanych w celu wyznaczenia przybliżonej objętości jeziora
+    % zmin - minimalna dopuszczalna wartość współrzędnej z losowanych punktów
+    % lake_volume - objętość jeziora wyznaczona metodą Monte Carlo
+
+    N = 1e6;
+    x = 100*rand(1,N); % [m]
+    y = 100*rand(1,N); % [m]
+    
+    zmin = [];
+    z = [];
+
+    lake_volume = [];
+
+end
